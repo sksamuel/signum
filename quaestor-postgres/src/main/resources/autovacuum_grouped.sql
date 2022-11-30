aaa5429cbbb28bfce80f276dd8ddf45cffc0c1e7
sksamuel <sam@sksamuel.com>
2022-11-29 23:17:57 -0600

    Added last_autoanalyze to group query

--- quaestor-postgres/src/main/resources/autovacuum_grouped.sql
+++ quaestor-postgres/src/main/resources/autovacuum_grouped.sql
@@ -1,5 +1,6 @@
 select max(last_autovacuum)   as last_autovacuum,
        sum(autovacuum_count)  as autovacuum_count,
-       sum(autoanalyze_count) as autoanalyze_count
+       sum(autoanalyze_count) as autoanalyze_count,
+       sum(last_autoanalyze)  as last_autoanalyze
 from pg_stat_user_tables
 WHERE relname LIKE :relname
