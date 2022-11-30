cdc0bce76c9817f77e08ba41c982c4b397f5a605
sksamuel <sam@sksamuel.com>
2022-11-30 07:32:25 -0600

    Added StatioMetrics metrics

---
+++ quaestor-postgres/src/main/resources/statio.sql
@@ -0,0 +1,10 @@
+select heap_blks_read,
+       heap_blks_hit,
+       idx_blks_read,
+       idx_blks_hit,
+       toast_blks_read,
+       toast_blks_hit,
+       tidx_blks_read,
+       tidx_blks_hit
+from pg_statio_user_tables
+WHERE relname like :relname
